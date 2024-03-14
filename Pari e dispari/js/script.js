312604031d35ab36a6be7fa4bf51e4c03d45b645
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-14 16:32:26 +0100

    feat: structure even or odd

---
+++ Pari e dispari/js/script.js
@@ -0,0 +1,17 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+// chiedere all'utente un numero da 1 a 5 e se sceglie pari o dispari
+
+// generare un numero random per il Pc da 1 a 5
+
+// sommare il numero utente e il numero Pc
+
+// se la somma dei numeri è pari e l'utente ha scelto pari 
+
+// stampiamo il vincitore utente
+// altrimenti scriviamo viceversa
