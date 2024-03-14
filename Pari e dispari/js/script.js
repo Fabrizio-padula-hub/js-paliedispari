60d4fb4732481b3aaa4523b3ef0d80945ba50e5a
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-14 17:40:48 +0100

    feat: addition of functions and user message

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -6,8 +6,10 @@
 
 
 // chiedere all'utente un numero da 1 a 5 e se sceglie pari o dispari
-const numberUser = 8;
-const evenoroddchoice = 'pari';
+const numberUser = parseInt(prompt('scrivi un numero da 1 a 5'));
+console.log(numberUser)
+const evenoroddchoice = prompt('scegli tra pari o dispari');
+console.log(evenoroddchoice)
 
 // generare un numero random per il Pc da 1 a 5
 const numberPc = Math.floor(Math.random() * 5) + 1;
@@ -15,13 +17,21 @@ console.log(numberPc)
 
 // sommare il numero utente e il numero Pc
 const sumNumUserPc = sumNumbers(numberUser, numberPc);
-console.log(sumNumUserPc)
+console.log('somma numeri', sumNumUserPc)
 
 // se la somma dei numeri è pari e l'utente ha scelto pari 
+// stampiamo il vincitore utente
+// altrimenti scriviamo viceversa
+const OutMessageUser = evenOrOdd(sumNumUserPc);
+console.log('messaggio utente', OutMessageUser)
+
+if(OutMessageUser === evenoroddchoice){
+    alert(`Hai scelto ${numberUser} e ${evenoroddchoice}, il Pc ha scelto ${numberPc}, la somma dei due numeri è ${sumNumUserPc}, quindi il risultato è ${OutMessageUser}, hai vinto`);
+}else{
+    alert(`Hai scelto ${numberUser} e ${evenoroddchoice}, il Pc ha scelto ${numberPc}, la somma dei due numeri è ${sumNumUserPc}, quindi il risultato è ${OutMessageUser}, hai perso`);
+}
 
 
-    // stampiamo il vincitore utente
-    // altrimenti scriviamo viceversa
 
 
 // FUNCTIONS
@@ -30,11 +40,19 @@ function sumNumbers(num1, num2){
     return sum;
 }
 
-let message;
-function evenorodd(num1, num2){
-    if(num1 % 2 === 0){
+
+// funzione che permette il calcolo di pari e dispari
+// prende un numero (number), viene eseguito un controllo
+// se il numero è divisibile per 2 senza lasciare resto
+// allora il numero è pari 
+// e la variabile message viene impostata su "pari"
+// se è dispari la variabile message viene impostata su "dispari".
+function evenOrOdd(number){
+    let message = '';
+    if(number % 2 === 0){
         message = 'pari';
     }else {
         message = 'dispari';
     }
+    return message;
 }
