c2165ae8f36e43bcb04713e0dd5ff41cd3fe4a4a
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-14 18:11:24 +0100

    feat: adding prompt and array

---
+++ palidroma/js/script.js
@@ -0,0 +1,19 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// chiedere all'utente una parola
+const askUser = prompt('Scrivi una parola');
+console.log(askUser)
+
+// verificare la parola
+    // creare un array
+    const listWord = [];
+
+    // inserire le lettere in un array
+for(let i = 0; i < askUser.length; i++){
+    console.log(askUser[i])
+
+}
+
+// controntare le lettere se la parola letta al contrario è uguale a quella scritta
