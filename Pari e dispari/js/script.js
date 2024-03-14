164e4c4125bc17855421a9f5358338bc95a7a739
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-14 16:51:21 +0100

    feat: add function

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -6,12 +6,35 @@
 
 
 // chiedere all'utente un numero da 1 a 5 e se sceglie pari o dispari
+const numberUser = 8;
+const evenoroddchoice = 'pari';
 
 // generare un numero random per il Pc da 1 a 5
+const numberPc = Math.floor(Math.random() * 5) + 1;
+console.log(numberPc)
 
 // sommare il numero utente e il numero Pc
+const sumNumUserPc = sumNumbers(numberUser, numberPc);
+console.log(sumNumUserPc)
 
 // se la somma dei numeri è pari e l'utente ha scelto pari 
 
-// stampiamo il vincitore utente
-// altrimenti scriviamo viceversa
+
+    // stampiamo il vincitore utente
+    // altrimenti scriviamo viceversa
+
+
+// FUNCTIONS
+function sumNumbers(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+
+let message;
+function evenorodd(num1, num2){
+    if(num1 % 2 === 0){
+        message = 'pari';
+    }else {
+        message = 'dispari';
+    }
+}
